feddf7187ceda68d313c4b39c1a4a2136c37e2a5
harikrishnan <harikrishnan.a@infibeam.net>
2021-06-10 19:48:23 +0530

    updated script

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const HCCrawler = require('headless-chrome-crawler');
+const CSVExporter = require('headless-chrome-crawler/exporter/csv');
 const fs = require('fs'); // v 5.0.0
+
 var argv = require('yargs')
   .usage('Usage: $0 [options]')
   .example('$0 -in url_list -out url_price_out', 'extracts price from www.flipkart.com and www.amazon.in urls')
@@ -21,29 +24,71 @@ if (argv.infile) {
   infile = fs.createReadStream(argv.infile);
 }
 
-const HCCrawler = require('headless-chrome-crawler');
-const CSVExporter = require('headless-chrome-crawler/exporter/csv');
+function extract() {
+  debugger
+  var domain = window.location.host
+  var item_json = {}
+  console.log("extracting...", domain)
+  var extractAmazon = function() {
+    console.log("extracting amazon...")
+    var title = document.getElementById('title').innerText.innerText
+    var offer_price = document.getElementById("priceblock_ourprice").innerText
+    var raw_price = document.getElementById('price').innerText
+    var item_json = {
+      "type": "Amazon",
+      "title": title,
+      "offer_price": offer_price,
+      "raw_price": raw_price
+    }
+    return item_json
+  }
+  var extractFlipkart = function() {
+    console.log("extracting flipkart...")
+  
+    var item_json = {
+      "type": "Flipkart",
+      "title": document.getElementsByTagName('h1')[0].innerText,
+      "offer_price": document.getElementsByClassName('_16Jk6d')[0].innerText,
+      "raw_price": document.getElementsByClassName('_16Jk6d')[0].innerText
+    }
+    return item_json
+  }
+  if (domain == 'www.amazon.in') {
+    item_json = extractAmazon()
+  }
+  else if (domain == 'www.flipkart.com') {
+    item_json = extractFlipkart()
+  }
+  debugger
+  return item_json
+}
 
 
 const exporter = new CSVExporter({
   file: outfile,
-  fields: ['response.url', 'response.status', 'result.title', 'result.price', 'result.raw_price'],
+  fields: ['result.type', 'response.url', 'response.status', 'result.title', 'result.offer_price', 'result.raw_price'],
+  separator: "\t"
 });
 
 (async () => {
   const crawler = await HCCrawler.launch({
     executablePath: '/usr/bin/google-chrome',
     headless: false,
-    evaluatePage: () => ({
-      title: '',
-      price: '',
-      raw_price: ''
-    }),
+    evaluatePage: extract,
+    waitUntil: 'networkidle2',
+    retryCount: 0,
+    jQuery: false,
+    requestfailed: (err) => {
+      console.err("Request failed! ", err)
+    },
     exporter
   });
-  infile.on('data', function (buf) {
-    buf.toString().split(/\n/).forEach(function (url) {
-      crawler.queue(url);
+
+  infile.on('data', async function (buf) {
+    buf.toString().split(/\n/).forEach(async function (url) {
+      if (url.trim().length) {
+        await crawler.queue(url);
+      }
     })
   });
   await crawler.onIdle();
